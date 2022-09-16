f6e539973163796ff96b178bd344b00ed8324369
Yifei Ma <634042441@qq.com>
2022-09-16 19:22:58 +0800

    Arrays.

--- funfamentals.js
+++ funfamentals.js
@@ -1,5 +1,13 @@
 const aCar = { 
     owner : "Joe Bloggs", 
+    address : "3 Walkers Lane",
+    
+    previous_owners: [
+        {name : "Pat Smith", address : "1 Main Street"},
+        {name : "Sheila Dwyer", address : "2 High Street"},
+    ],
+
+
     type :{
         make : "Toyota",
         model : "Corolla",
@@ -20,9 +28,11 @@ const aCar = {
             texture: "leather",
             shade : "cream"
         }
-    }
+    },
+
 };
-console.log(aCar.owner);
-console.log(aCar.owner + ' drives a ' + aCar.type.make)
-console.log(aCar.registration.year + '-' + aCar.registration.county_code + '-' + aCar.registration.number)
-console.log( "It is a " + aCar.color.exterior + " car, " + aCar.milage + " milage, with " + aCar.color.interior.texture + " interior.")
+// console.log(aCar.owner);
+// console.log(aCar.owner + ' drives a ' + aCar.type.make)
+// console.log(aCar.registration.year + '-' + aCar.registration.county_code + '-' + aCar.registration.number)
+// console.log( "It is a " + aCar.color.exterior + " car, " + aCar.milage + " milage, with " + aCar.color.interior.texture + " interior.")
+console.log('First owner : ' + aCar.previous_owners[0].name + "-"+ aCar.previous_owners[0].address);
