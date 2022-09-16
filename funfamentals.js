5e2e5659d82f31872ac0d633ad285635d77c432d
Yifei Ma <634042441@qq.com>
2022-09-16 19:12:34 +0800

    Dynamic properties.

--- funfamentals.js
+++ funfamentals.js
@@ -10,9 +10,19 @@ const aCar = {
         year : 201,
         county_code: "WD",
         number : 1058
-    } 
+    } ,
+
+    milage : 10000,
+
+    color :{
+        exterior : "red",
+        interior : {
+            texture: "leather",
+            shade : "cream"
+        }
+    }
 };
 console.log(aCar.owner);
 console.log(aCar.owner + ' drives a ' + aCar.type.make)
 console.log(aCar.registration.year + '-' + aCar.registration.county_code + '-' + aCar.registration.number)
-
+console.log( "It is a " + aCar.color.exterior + " car, " + aCar.milage + " milage, with " + aCar.color.interior.texture + " interior.")
